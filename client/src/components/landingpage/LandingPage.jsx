45101aba337e87a26442db18505a94d07b2e5bdf
JaniHans <laurihanstin@gmail.com>
2025-11-18 18:38:08 +0100

    topbar added and designed, body text and buttons redone , message box to do and AI to integrate, data retrieved , data to map into a table or form preferably

--- client/src/components/landingpage/LandingPage.jsx
+++ client/src/components/landingpage/LandingPage.jsx
@@ -9,10 +9,9 @@ function LandingPage() {
     const [isError, setIsError] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [responseStatus, setResponseStatus] = useState(0);
-    const [industry, setIndustry] = useState({});
-    const [value, setValue] = useState();
-
-    console.log(value)
+    const [industry, setIndustry] = useState();
+    const [industryCode, setIndustryCode] = useState("");
+    const [industrySalary, setIndustrySalary] = useState([]);
 
     /**
      * Transform this into a map so we can use later for queries, we need to match ids with economic activities
@@ -38,6 +37,13 @@ function LandingPage() {
 
     }
 
+    /**
+     *
+     * @returns {Promise<void>}
+     */
+    function addIndustrySalaryByYearlyToArray(salaryArrayYearly) {
+
+    }
     const getUrlQueryForDifferentDatabases = async () => {
         try {
             const response = await fetch("https://andmed.stat.ee/api/v1/en/stat/majandus/palk-ja-toojeukulu/palk/aastastatistika")
@@ -56,16 +62,14 @@ function LandingPage() {
     }
 
     /**
-     * @URL - https://andmed.stat.ee/api/v1/en/stat/majandus/palk-ja-toojeukulu/palk/aastastatistika
+     * @URL -source https://andmed.stat.ee/api/v1/en/stat/majandus/palk-ja-toojeukulu/palk/aastastatistika
      * @POST Query - PA103
      * @returns {Promise<void>}
      */
-    const getStructureOfTheDb = async () => {
+    const categorizeIndustriesByActivityAndCodes = async () => {
         try {
             const response = await fetch("https://andmed.stat.ee/api/v1/en/stat/PA103")
             if (!response.ok) {
-                console.log("Where is the response")
-            console.log(response)
                 throw new Error(`Response status :  ${setResponseStatus(response.status)}`)
             }
             const result = await response.json();
@@ -94,7 +98,7 @@ function LandingPage() {
                         "selection" : {
                             "filter" : "item",
                             "values" : [
-                                'J63'
+                                `${industryCode}`
                             ]
                         }
                     },
@@ -103,20 +107,10 @@ function LandingPage() {
                             "selection" : {
                                 "filter" : "item",
                                 "values" : [
-                                    'GR_W_AVG'
+                                    'GR_W_AVG', 'GR_W_AVG_SM'
                                 ]
                             }
                         },
-                        {
-                            "code" : "Vaatlusperiood",
-                            "selection" : {
-                                "filter" : "item",
-                                "values" : [
-                                    '2024'
-                                ]
-                            }
-                        }
-
                 ],
                     "response" : {
                         "format" : "json-stat2"
@@ -162,42 +156,52 @@ function LandingPage() {
                     }, 2)
                 }
             }, 1000)
-
     }
 
     function handleIndustryChange(e) {
        if (e.target.value !== "") {
-           setValue(e.target.value)
+           setIndustryCode(e.target.value)
        }
-
     }
 
 
     return (
         <div className="main-container">
+
             <div className="main-container-items">
-                <div className="buttons">
-                    <button onClick={loadServerMessage}>LOAD SERVER FOR FUN</button>
-                    <button onClick={loadServerMessage}>Get Different DATABASES</button>
-                    <button onClick={loadServerMessage}>LOAD AI</button>
-                    <button onClick={getStructureOfTheDb}>LOAD DB</button>
-                    <button onClick={getSalaryByCategory}>LOAD BY CATEGORY</button>
+                <div className="text-container">
+                <h2>Average salaries in Estonia powered by Statistics Estonia and OpenAI</h2>
+                <p>Get industry average salaries for the last 4 years</p>
+                <p>Use AI to transform your career in the right direction</p>
+                    <div className="buttons">
+                        <button id="left-button" onClick={loadServerMessage}>LOAD SERVER</button>
+                        {/*<button onClick={getUrlQueryForDifferentDatabases}>Get Different DATABASES</button>*/}
+                        <button onClick={loadServerMessage}>LOAD AI</button>
+                        <button onClick={categorizeIndustriesByActivityAndCodes}>LOAD DB</button>
+                        <button onClick={getSalaryByCategory}>LOAD SALARIES</button>
+                    </div>
+                    <div hidden={!industry}>
+                        <label htmlFor="industry-select">Select industry</label>
+                        <select id="industry-select" name="industry-names" onChange={(e) => handleIndustryChange(e)}>
+                            <option selected disabled>---Choose an option---</option>
+                            {
+                                industry && Object.entries(industry).map(([key, value]) =>
+                                    <option key={key} value={key.toString()}>{value}</option>
+                                )
+                            }
+                        </select>
+
+                    </div>
+                </div>
+
+
+
                 </div>
                 <div className="message-container">
-                    {isLoading ? <p className="loading">Loading...</p> : null}
-                    {isError ? <p className="error">Status code : {responseStatus} - {errorMessage}</p> : null}
+                {isLoading ? <p className="loading">Loading...</p> : ""}
+                {isError ? <p className="error">Status code : {responseStatus} - {errorMessage}</p> : ""}
                 </div>
             </div>
-            <label htmlFor="">Choose Industry</label>
-            <select name="industry-names" onChange={(e) => handleIndustryChange(e)}>
-                <option selected disabled>---Pick industry---</option>
-                {
-                    industry && Object.entries(industry).map(([key, value]) =>
-                    <option key={key} value={key.toString()}>{value}</option>
-                    )
-                }
-            </select>
-        </div>
 
     );
 }
---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './topbar.css';
+
+function TopBar() {
+    return (
+        <div className="topbar">
+            <h1>Äripäev</h1>
+        </div>
+    );
+}
+
+export default TopBar;
